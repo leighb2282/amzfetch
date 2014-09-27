c8870151e9e9b17ee14cc64ae286d158fbc72c8d
Leigh Burton <lburton@metacache.net>
2014-09-26 22:40:50 -0700

    cleared up useless code.
    Started adding in the code to parse the input file.

--- amzfetch.py
+++ amzfetch.py
@@ -2,7 +2,9 @@
 # amzfetch.py
 # Description: Tool for downloading Amazon MP3s using a .amz file.
 # Version: 0.00.00
-# Last Updated: Fri 26 Sep 2014 11:02:38 AM PDT 
+# Last Updated: Fri 26 Sep 2014 10:40:34 PM PDT  
+# Leigh Burton, lburton@metacache.net
+# Status: it just dumps the input file to console.
 
 """amzfetch.py - A tool for downloading Amazon MP3s as detailed in a .amz file. 
    
@@ -13,19 +15,18 @@ import sys
 import argparse
  
 def main(arguments):
- 
-    parser = argparse.ArgumentParser(description=__doc__,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('infile', help = "Input file",
-                        type = argparse.FileType('r'))
- 
-    args = parser.parse_args(arguments)
-    
-    print args
-script, input_file = sys.argv
-in_file = open(input_file)
-print in_file.read()
-	
+
+    script, input_file = sys.argv
+    in_file = open(input_file)
+    prop = in_file.read()
+
+    striped = prop.strip( )
+    print striped
+
+#for line in prop:
+#    line2 = line.strip( )
+#    print line2
+
     
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
