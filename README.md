6da39233e059f2ab0cda87272681416e19297673
Leigh Burton <lburton@metacache.net>
2014-10-06 22:53:35 -0700

    amzfetch v0.25.00 - Fully re-wrote to take advantage of in-toll opening of files, download location selection. etc.

--- README.md
+++ README.md
@@ -5,41 +5,36 @@ Tool to pull down Amazon MP3s via a .amz (XML) file.
 
 Requires TkInter, can be installed via your Distro's package manager)
 
-amzfetch.py v0.24.00
+amzfetch.py v0.25.00
 ------------------------------------
 
-![alt tag](http://amzfetch.metacache.net/github/v0.23.00.png)
+![alt tag](http://amzfetch.metacache.net/github/v0.25.00.png)
 
-New GUI implemented via TkInter, with information pertaining to the download of mp3s now outputting to a console window alongside logging to a log file (which you can set in the logloc variable at the top of the source.
+New GUI implemented via TkInter, with information pertaining to the download of mp3s now outputting to a console window.
 
-User invokes the script as follows:
-    python amzfetch.py pathto.amz
+User invokes the script as follows (no arguments required):
+    python amzfetch.py
     
-This version is very basic, but it DOES work, (it has been tested multiple times via actual .amz files from amazon digital store.
+User can select an .amz for anywhere on the filesystem, once they have decided to download the MP3s from the .amz file they can choose a location to have the mp3s downloaded to.
 
 There are multiple improvements I can probably do even on this code alone (the download handling for instance, I already know of a more elegent way of doing that, these kind of changes will be integrated into v0.30.00 code base.
 
-v0.24.00 KNOWN ISSUES
+v0.25.00 KNOWN ISSUES
 ---------------------
 
+* If you close the console window before using the quit button on the main window it will error.
 * The URLs in the .amz file expire after a certain amount of time, if you try to use an expired .amz file the app will error.
-* I need to impliment some form of file detection to make sure it only runs when fed the correct type of file (.amz)
+* I need to impliment some form of file detection to make sure it only runs when fed the correct type of file (.amz) as amzfetch will error if a non.amz file is selected.
 
 Planned future improvements
 ---------------------------
 
-* Allow selection of .amz file from GUI, not command line
 * Allow limited selection of specific files for download (selective downloads)
-* Allow user to select location for downloaded files, toggle creation of folders or simple download location.
+* Add an additional download option which automatically downloads the mp3s to a downloads folder in the scripts location versus choosing a location.
+* Improve file input handling (limit files in the file dialog to just .amz files, stop showing all files - including hiddne folders/files)
+* Allow one instance of the tool to download multiple .amz files - currently to feed it another .amz file you will need to close and re-open the tool.
+* fix the above error where if you close the console window before using the quit button on the main window it will error.
 * Allow synchronous download of files.
-* Allow the script to create the directory structure for your files (Artist/Album/mp3s).
 * Integrate Keen.io API to track events through the work flow cycle (open the source file, Gather needed data on tracks & total % of tracks, completion of each of those mp3s).
-* All above + complete bug fixes. (v1.00.00)
-
-Thoughts on improving GUI
------------------------
-
-1. allow selection of .amz file from GUI, not command line
-2. allow limited selection of specific files for download (selective downloads)
-3. allow user to select location for downloaded files, toggle creation of folders or simple download location.
+* All above improvements + Known issues + complete bug fixes. (v1.00.00)
 
--- amzfetch.py
+++ amzfetch.py
@@ -1,114 +1,133 @@
 #!/usr/bin/env python
 # amzfetch.py
 # Description: Tool for downloading Amazon MP3s using a .amz file.
-# Version: 0.24.00
-# Last Updated: Sun 05 Oct 2014 07:40:44 PM PDT    
+# Version: 0.25.00
+# Last Updated: Mon 06 Oct 2014 10:43:07 PM PDT 
 # Leigh Burton, lburton@metacache.net
-# Status: GUI via TkInter, Displays list of MP3s in the .amz file, user can then download them. 
+# Status: Full Re-write in process, to allow in-app loading of files.
  
 import os
 import sys
-import argparse
 import urllib2
 from xml.dom import minidom
 from Tkinter import *
-from time import strftime
+from tkFileDialog import askopenfilename
+from tkFileDialog import askdirectory
 
 
-toolv = 'v0.24.00'
-logloc = 'amzfetch.log'
-
+toolv = 'v0.25.00'
 
 def main(arguments):
-    console = Tk()
-    console.title('Console Output')
     
+    filename = ''
+    itemtotal = ''
+    
+    # function to kill the open windows.
     def allquit():
-        conlog = open(logloc, 'a+')
-        conlog.write(strftime("%Y-%m-%d %H:%M:%S") + ' ' + args.infile + ' Closed.\n')
-        conlog.close()
         console.destroy()
         root.destroy()
-        
-    root = Tk()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('infile', help = "Input file (.amz)")
-    args = parser.parse_args(arguments)
-
-    with open(args.infile, 'r') as input_file:
-        xmldoc = minidom.parse(input_file)
-        itemloc = xmldoc.getElementsByTagName('location')
-        itemtitle = xmldoc.getElementsByTagName('title')
-        itemtnum = xmldoc.getElementsByTagName('trackNum')
-        itemcreator = xmldoc.getElementsByTagName('creator')
-        itemalbum = xmldoc.getElementsByTagName('album')
-     
-    itemtotal = len(itemloc)
-    itemfoo = 0
-    label = Label(console, text=str(itemtotal) + ' songs in ' + args.infile, fg="#357EC7").grid(sticky=W)
     
-    conlog = open(logloc, 'a+')
-    conlog.write(strftime("%Y-%m-%d %H:%M:%S") + ' ' + args.infile + ' Opened. ' + str(itemtotal) + ' songs in ' + args.infile + '\n')
-    conlog.close()
-        
+    # Function that downloads files.
     def urldl():
+        with open(main.filename, 'r') as input_file:
+            xmldoc = minidom.parse(input_file)
+            itemloc = xmldoc.getElementsByTagName('location')
+            itemtitle = xmldoc.getElementsByTagName('title')
+            itemtnum = xmldoc.getElementsByTagName('trackNum')
+            itemcreator = xmldoc.getElementsByTagName('creator')
+            itemalbum = xmldoc.getElementsByTagName('album')
+        itemtotal = len(itemloc)
+        
+        console.update()
         itemfoo = 0
-        label = Label(console, text='Starting Download of ' + str(itemtotal) + ' songs', fg="red").grid(sticky=W)
-        
-        conlog = open(logloc, 'a+')
-        conlog.write(strftime("%Y-%m-%d %H:%M:%S") + ' ' + args.infile + ' Starting Download of ' + str(itemtotal) + ' songs\n')
-        conlog.close()
-        
-        console.update()
-        while itemfoo != itemtotal:
-            
-            mp3url = itemloc[itemfoo].childNodes[0].nodeValue
-            mp3title = itemtitle[itemfoo + 1].childNodes[0].nodeValue
-            mp3tnum = itemtnum[itemfoo].childNodes[0].nodeValue
-            mp3creator = itemcreator[itemfoo].childNodes[0].nodeValue
-            mp3album = itemalbum[itemfoo].childNodes[0].nodeValue
-            response = urllib2.urlopen(mp3url)
-            mp3out = response.read()
-            with open(mp3tnum + ' ' + mp3title + '.mp3', 'wb') as newfile:
-                newfile.write(mp3out)
-                newfile.close()
-            label = Label(console, text=str(mp3title) + ' by ' + str(mp3creator) + ' Downloaded').grid(sticky=W)
+        downloc = askdirectory()
+        if downloc == '':
+            print ''
+        else:
+            label = Label(console, text='Starting Download of ' + str(itemtotal) + ' songs', fg="red").grid(sticky=W)
+            label = Label(console, text='Files will be in ' + downloc, fg="red").grid(sticky=W)
             console.update()
-            
-            conlog = open(logloc, 'a+')
-            conlog.write(strftime("%Y-%m-%d %H:%M:%S") + ' ' + args.infile + ' ' + str(mp3title) + ' by ' + str(mp3creator) + ' Downloaded\n')
-            conlog.close()
-            
+            while itemfoo != itemtotal:
+                mp3url = itemloc[itemfoo].childNodes[0].nodeValue
+                mp3title = itemtitle[itemfoo + 1].childNodes[0].nodeValue
+                mp3tnum = itemtnum[itemfoo].childNodes[0].nodeValue
+                mp3creator = itemcreator[itemfoo].childNodes[0].nodeValue
+                mp3album = itemalbum[itemfoo].childNodes[0].nodeValue
+                response = urllib2.urlopen(mp3url)
+                mp3out = response.read()
+                with open(downloc + '/' + mp3tnum + ' ' + mp3title + '.mp3', 'wb') as newfile:
+                    newfile.write(mp3out)
+                    newfile.close()
+                itemfoo = itemfoo + 1
+                label = Label(console, text=unicode(mp3title).encode("utf-8") + ' by ' + unicode(mp3creator).encode("utf-8") + ' Downloaded').grid(sticky=W)
+                console.update()
+            label = Label(console, text='All Items Downloaded', fg="red").grid(sticky=W)
+            console.update()
+    
+    # function to select the amzfile.
+    def openamz():
+        main.filename = askopenfilename()
+        if main.filename == '':
+            print ''
+        else:
+            with open(main.filename, 'r') as input_file:
+                xmldoc = minidom.parse(input_file)
+                itemloc = xmldoc.getElementsByTagName('location')
+                itemtitle = xmldoc.getElementsByTagName('title')
+                itemtnum = xmldoc.getElementsByTagName('trackNum')
+                itemcreator = xmldoc.getElementsByTagName('creator')
+                itemalbum = xmldoc.getElementsByTagName('album')
+     
+            itemtotal = len(itemloc)
+            itemfoo = 0
+            while itemfoo != itemtotal:
+                mp3url = itemloc[itemfoo].childNodes[0].nodeValue
+                mp3title = itemtitle[itemfoo + 1].childNodes[0].nodeValue
+                mp3tnum = itemtnum[itemfoo].childNodes[0].nodeValue
+                mp3creator = itemcreator[itemfoo].childNodes[0].nodeValue
+                mp3album = itemalbum[itemfoo].childNodes[0].nodeValue
+                itemfoo = itemfoo + 1
+                lartist = Label(root, text='Artist: ', fg="#357EC7").grid(row=itemfoo, column=0, sticky=E)
+                dartist = Label(root, text=unicode(mp3creator).encode("utf-8")).grid(row=itemfoo, column=1, sticky=W)
+                ltitle = Label(root, text='Song: ', fg="#357EC7").grid(row=itemfoo, column=2, sticky=E)
+                dtitle = Label(root, text='(' + str(mp3tnum) + ') ' + unicode(mp3title).encode("utf-8")).grid(row=itemfoo, column=3, sticky=W)
+                lalbum = Label(root, text='Album: ', fg="#357EC7").grid(row=itemfoo, column=4, sticky=E)
+                dalbum = Label(root, text=str(mp3album)).grid(row=itemfoo, column=5, sticky=W)
             itemfoo = itemfoo + 1
-        label = Label(console, text='All Items Downloaded', fg="red").grid(sticky=W)
+            dlurl = Button(root, text='Download Songs', command=urldl).grid(row=0, column=1, sticky=W)
+            label = Label(console, text=main.filename + ' Opened', fg="#357EC7").grid(sticky=W)
         
-        conlog = open(logloc, 'a+')
-        conlog.write(strftime("%Y-%m-%d %H:%M:%S") + ' ' + args.infile + ' All Items Downloaded\n')
-        conlog.close()
-        
+            root.update()
+
+    #test function.    
+    def test():
+        label = Label(console, text=str(main.filename), fg="#357EC7").grid(sticky=W)
         console.update()
-        
-    while itemfoo != itemtotal:
-        mp3url = itemloc[itemfoo].childNodes[0].nodeValue
-        mp3title = itemtitle[itemfoo + 1].childNodes[0].nodeValue
-        mp3tnum = itemtnum[itemfoo].childNodes[0].nodeValue
-        mp3creator = itemcreator[itemfoo].childNodes[0].nodeValue
-        mp3album = itemalbum[itemfoo].childNodes[0].nodeValue
-        
-        lartist = Label(root, text='Artist: ', fg="#357EC7").grid(row=itemfoo, column=0, sticky=E)
-        ltitle = Label(root, text='Song: ', fg="#357EC7").grid(row=itemfoo, column=2, sticky=E)
-        lalbum = Label(root, text='Album: ', fg="#357EC7").grid(row=itemfoo, column=4, sticky=E)
-        dartist = Label(root, text=str(mp3creator)).grid(row=itemfoo, column=1, sticky=W)
-        dtitle = Label(root, text='(' + str(mp3tnum) + ') ' + str(mp3title)).grid(row=itemfoo, column=3, sticky=W)
-        dalbum = Label(root, text=str(mp3album)).grid(row=itemfoo, column=5, sticky=W)
-        itemfoo = itemfoo + 1
     
-    dlquit = Button(root, text='Quit', command=allquit).grid(row=itemfoo, column=0, sticky=E)
-    dlurl = Button(root, text='Download Songs', command=urldl).grid(row=itemfoo, column=1, sticky=E)
-    root.title('amzfetch.py ' + toolv + ' :: ' + args.infile)
+
+
+    
+    ##########################
+    # Initial App stuff here #
+    ##########################
+    
+    # Set up windows.
+    console = Tk()
+    root = Tk()
+    root.geometry('+100+100')
+    console.minsize(300,200)
+    console.geometry('+100+300')
+    # Set the window titles.
+    console.title('amzfetch.py :: Console Output')
+    root.title('amzfetch.py ' + toolv + ' :: ')
+    
+    # Create the initial widgets (Quit, Open).
+    dlquit = Button(root, text='Quit', command=allquit).grid(row=0, column=0, sticky=E)
+    dlopen = Button(root, text='Open', command=openamz).grid(row=0, column=1, sticky=W)
+    # Create initial windows.
     root.mainloop()
-    
     console.mainloop()
+    
     
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
